61c5c34b96166aff2686455fe830067c0971e0b8
Rohithossain12 <https://github.com/Rohithossain12>
2025-01-06 06:23:17 +0600

    create delete api for cart item

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete cart item
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
