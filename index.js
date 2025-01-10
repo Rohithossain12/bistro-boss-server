57e4b6ef135562d2e076b12b1613bcbebe113809
Rohithossain12 <https://github.com/Rohithossain12>
2025-01-10 16:37:10 +0600

    create payment history api for specific user by email

--- index.js
+++ index.js
@@ -219,6 +219,16 @@ async function run() {
       });
     });
 
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      if (!req.decoded || req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+
+      const query = { email: req.params.email };
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/payments", async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
